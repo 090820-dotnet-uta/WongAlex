3188024bd9d058de8a90f36e9e6a984ecd3d6ab9
Alex <alwong34@uw.edu>
2020-09-08 14:04:42 -0700

    change helloWorld to take in input and print it

--- HelloWorldDemo/Program.cs
+++ HelloWorldDemo/Program.cs
@@ -7,6 +7,10 @@ namespace HelloWorldDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Alex!");
+            System.Console.WriteLine("What is your name?");
+            var input = Console.ReadLine();
+            System.Console.WriteLine($"Hello {input}");
+            System.Console.WriteLine("Hello {0}", input);
         }
     }
 }
